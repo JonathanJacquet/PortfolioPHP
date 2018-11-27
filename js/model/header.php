b1bab61e216f0bdb3b39d935d77c8325a85d0b5f
Jo <jonathan.jacquet62@laposte.net>
2018-11-27 11:20:27 +0100

    first commit

---
+++ js/model/header.php
@@ -0,0 +1,45 @@
+<div class="contener_column">
+      <div class="column"><a href="taf.html">
+          <article>
+          <h1>TWITTER</h1>
+          <hr>
+          <p>Entraînement au positionnement, en utilisant flex box.</p>
+        </article>
+        </a>
+      </div>
+      <div class="column"><a href="taf.html">
+        <article>
+          <h1>PATISSERIE EMMANUELLE</h1>
+          <hr>
+          <p>Réaliser la home page responsive d'un site de pâtisserie</p>
+        </article>
+        </a>
+      </div>
+      <div class="column"><a href="taf.html">
+        <img src="" alt="">
+        <article>
+          <h1>Titre</h1>
+          <hr>
+          <p>Texte texte texte</p>
+        </article>
+        </a>
+      </div>
+      <div class="column"><a href="taf.html">
+        <img src="" alt="">
+        <article>
+          <h1>Titre</h1>
+          <hr>
+          <p>Texte texte texte</p>
+        </article>
+        </a>
+      </div>
+      <div class="column"><a href="taf.html">
+        <img src="" alt="">
+        <article>
+          <h1>Titre</h1>
+          <hr>
+          <p>Texte texte texte</p>
+        </article>
+        </a>
+      </div>
+    </div>
